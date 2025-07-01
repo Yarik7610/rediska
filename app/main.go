5d2b31c2091116ae5684dfc53d1dee983974154f
Yaroslav Yanovich <arikarik782@gmail.com>
2025-07-01 17:53:04 +0300

    feat: add hardcoded +PONG response

--- app/main.go
+++ app/main.go
@@ -5,14 +5,26 @@ import (
 	"net"
 )
 
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	conn.Write([]byte("+PONG\r\n"))
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		log.Fatalln("Failed to bind to '0.0.0.0:6379'")
 	}
+	defer listener.Close()
 
-	_, err = listener.Accept()
-	if err != nil {
-		log.Fatalf("Error accepting connection: %v\n", err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v\n", err)
+			continue
+		}
+
+		go handleClient(conn)
 	}
 }
